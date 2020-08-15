8d71f4aa92a18d34a338377b73c7f4c429abd3f6
Yashmerino <artiomytprofile@gmail.com>
2020-08-16 00:43:18 +0300

    Added commentaries

--- Dungeon Master RPG.cpp
+++ Dungeon Master RPG.cpp
@@ -3,33 +3,33 @@
 
 using namespace std;
 
-class DungeonMaster {
+class DungeonMaster { //Dungeon Master class
 private:
-    string name = "noname";
+    string name = "noname"; //Vars
     char sex = ' ';
     int age = 0;
     int weight = 0;
 
 public:
-    void SetName() {
+    void SetName() { //Setter for the name
         string nick;
         cin >> nick;
         name = nick;
     }
 
-    void SetAge() {
+    void SetAge() { //Setter for the age
         int value;
         cin >> value;
         age = value;
     }
 
-    void SetWeight() {
+    void SetWeight() { //Setter for the weight
         int value;
         cin >> value;
         weight = value;
     }
 
-    void SetSex() {
+    void SetSex() { //Setter for the sex
         cin >> sex;
         while (sex != 'f' && sex != 'm' && sex != 'M' && sex != 'F') {
             cout << "Error. Only F or M: ";
@@ -37,7 +37,7 @@ public:
         }
     }
 
-    void PrintInfo() {
+    void PrintInfo() { //Function to print info about the character
         cout << endl;
         cout << "Your character's name is: " << name << ".\nIt is " << age << " years old." << "\nIt weighs " << weight << " kg." << "\nIt's sex is: " << (char)toupper(sex) << endl;
     }
@@ -46,20 +46,20 @@ public:
 
 int main()
 {
-    DungeonMaster hero;
+    DungeonMaster hero; //Creatting hero
     cout << "Welcome to the Dungeon Master RPG.\nCreate your character.\nType a name for it: ";
-    hero.SetName();
+    hero.SetName(); //Setting it's name
 
     cout << "It's age: ";
-    hero.SetAge();
+    hero.SetAge(); //Setting it's age
 
     cout << "It's weight: ";
-    hero.SetWeight();
+    hero.SetWeight(); //Setting it's weight
 
     cout << "It's sex (only F or M): ";
-    hero.SetSex();
+    hero.SetSex(); //Setting it's sex
 
-    hero.PrintInfo();
+    hero.PrintInfo(); //Printing info about the character
     
     cin.get();
     return 0;
